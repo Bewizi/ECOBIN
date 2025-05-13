929ee05ac64ece36953ed407676da2a2c6d1753d
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-13 17:28:06 +0100

    refactor: remove unused imports from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-// import Home from "@/pages/Home.vue";
-// import Requests from "./pages/Requests.vue";
-// import { Page } from "@nativescript/core";
 import OnboardingSlides from "./components/onboarding/OnboardingSlides.vue";
 </script>
 
