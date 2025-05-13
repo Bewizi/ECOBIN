d7d1666cb7d4955e6356cab327019a0fe3efe318
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-13 05:14:13 +0100

    feat: add CardView component for reusable card layout

---
+++ src/components/CardView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { FlexboxLayout } from "@nativescript/core";
+
+defineProps<{
+  className?: string;
+}>();
+</script>
+
+<template>
+  <StackLayout class="bg-[#F6F7F9] px-6 rounded-lg mb-5 p-4">
+    <FlexboxLayout :class="className || 'justify-between items-center'">
+      <slot />
+    </FlexboxLayout>
+  </StackLayout>
+</template>
+
+<style scoped></style>
