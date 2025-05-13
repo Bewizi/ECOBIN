119d9427446071ada8f54274d8e98f3df005a3fb
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-13 13:18:27 +0100

    fix: add newline for improved code readability

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import "@/app.css";
 import App from "@/App.vue";
 
 const app = createApp(App);
+
 app.registerElement(
   "CheckBox",
   () => require("@nativescript-community/ui-checkbox").CheckBox,
