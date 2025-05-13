7ccb46761fff9ed63bf1f4adc160aed889afa348
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-13 05:16:37 +0100

    feat: add Activity page with layout and navigation components

---
+++ src/pages/Activity.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  DockLayout,
+  ScrollView,
+  StackLayout,
+  WrapLayout,
+} from "@nativescript/core";
+import BottomTabs from "~/components/BottomTabs.vue";
+</script>
+
+<template>
+  <Page actionBarHidden="true">
+    <DockLayout>
+      <BottomTabs />
+
+      <ScrollView>
+        <StackLayout class="pt-10 px-4">
+          <Label
+            class="text-4xl font-semibold text-center text-black"
+            text="Welcome To Activity Page hi"
+          >
+          </Label>
+
+          <WrapLayout>
+            <Label text="click to go back home" />
+            <Label text="wassup" />
+            <Label text="youtube" />
+          </WrapLayout>
+        </StackLayout>
+      </ScrollView>
+    </DockLayout>
+  </Page>
+</template>
+
+<style scoped></style>
