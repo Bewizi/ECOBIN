953c4f479d4079b507a1406e947c951f22e443b8
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-13 05:15:28 +0100

    feat: add PickUpDetails component for displaying waste pickup information

---
+++ src/views/PickUpDetails.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {
+  FlexboxLayout,
+  FormattedString,
+  Label,
+  Page,
+  ScrollView,
+  StackLayout,
+} from "@nativescript/core";
+import CardView from "~/components/CardView.vue";
+</script>
+
+<template>
+  <Page actionBarHidden="true">
+    <StackLayout class="px-5 pt-10">
+      <Label text="Waste Pickup" />
+      <CardView class="border-2 border-gray-600">
+        <FlexboxLayout className="justify-between items-center">
+          <StackLayout>
+            <Label textWrap="true" text="Address" />
+          </StackLayout>
+          <StackLayout>
+            <Label textWrap="true">
+              <FormattedString>
+                <Span text="03, Adekunmi Estate,\n" />
+                <Span text="Ojota, Lagos" />
+              </FormattedString>
+            </Label>
+          </StackLayout>
+        </FlexboxLayout>
+      </CardView>
+    </StackLayout>
+  </Page>
+</template>
+
+<style scoped></style>
