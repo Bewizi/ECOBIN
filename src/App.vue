dec7eece94f8f3ccaff672c3642439a77d1da21d
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-13 20:51:46 +0100

    feat:removed unsable components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import SignUp from "./Auth/SignUp.vue";
+import SuccessModal from "./components/modal/SuccessModal.vue";
 import OnboardingSlides from "./components/onboarding/OnboardingSlides.vue";
 </script>
 
 <template>
   <Frame>
+    <!-- <SignUp /> -->
+    <!-- <SuccessModal /> -->
     <OnboardingSlides />
   </Frame>
 </template>
