e84b8684004c911dffd8b0f086e4d0833acbbe8b
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-13 20:53:38 +0100

    feat:refactored successmodal

--- src/components/modal/SuccessModal.vue
+++ src/components/modal/SuccessModal.vue
@@ -1,45 +1,71 @@
 <script setup lang="ts">
-import { $navigateTo } from "nativescript-vue";
+import { FlexboxLayout, StackLayout } from "@nativescript/core";
+import { $closeModal, $navigateTo } from "nativescript-vue";
 import Home from "~/pages/Home.vue";
+
+// defineProps({
+//   completeSetup: {
+//     type: Function,
+//     required: true,
+//   },
+//   skipForNow: {
+//     type: Function,
+//     required: true,
+//   },
+// });
+
+// completeSetup($navigateTo(Home, { clearHistory: true }))
 
 const completeSetup = () => {
   $navigateTo(Home, { clearHistory: true });
+  $closeModal();
 };
 
 const skipForNow = () => {
   $navigateTo(Home, { clearHistory: true });
+  $closeModal();
 };
 </script>
 
 <template>
-  <Page actionBarHidden="true">
-    <StackLayout class="p-8">
-      <!-- Success Icon -->
-      <Label text="✓" class="text-[#54B469] text-6xl text-center mb-6" />
-
+  <StackLayout class="px-8 py-10 bg-white rounded-xl w-full">
+    <!-- Success Icon -->
+    <StackLayout>
       <Label
-        text="Success! You're Almost There 🎉"
-        class="text-3xl font-bold text-center mb-4"
-        textWrap="true"
-      />
-
-      <Label
-        text="Thanks for signing up. Let's set up your account so we can tailor your EcoBin experience to you!"
-        class="text-gray-600 text-center mb-8"
-        textWrap="true"
-      />
-
-      <Button
-        text="Complete Setup"
-        class="bg-[#54B469] text-white p-4 rounded mb-4"
-        @tap="completeSetup"
-      />
-
-      <Button
-        text="Skip for Now"
-        class="bg-transparent text-[#54B469] p-4"
-        @tap="skipForNow"
+        fontSize="26"
+        text="&#xf00c;"
+        width="50"
+        height="50"
+        class="fa bg-[#54B469] text-white rounded-full text-center mb-6"
       />
     </StackLayout>
-  </Page>
+
+    <Label
+      fontSize="24"
+      text="Success! You're Almost There 🎉"
+      class="font-bold text-center mb-4 text-black"
+      textWrap="true"
+    />
+
+    <Label
+      fontSize="14"
+      text="Thanks for signing up. Let's set up your account so we can tailor your EcoBin experience to you!"
+      class="text-[#585E6D] text-center mb-8"
+      textWrap="true"
+    />
+
+    <Button
+      text="Skip for Now"
+      fontSize="14"
+      class="bg-transparent font-medium border-2 border-[#54B469] mb-4 rounded-xl text-[#54B469] p-4 shadow-none"
+      @tap="skipForNow"
+    />
+
+    <Button
+      text="Complete Setup"
+      fontSize="14"
+      class="bg-[#54B469] font-medium text-white p-4 rounded-xl"
+      @tap="completeSetup"
+    />
+  </StackLayout>
 </template>
