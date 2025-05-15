c64caf768c6e10c7ca3cfd65734f42f061f2bd95
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-15 16:05:55 +0100

    fix: removed unused config which cause confict

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const webpack = require("@nativescript/webpack");
 module.exports = (env) => {
   webpack.init(env);
 
+  // Learn how to customize:
+  // https://docs.nativescript.org/webpack
+
   webpack.chainWebpack((config) => {
     config.devServer.hotOnly(true).hot(true);
   });
-
-  // Learn how to customize:
-  // https://docs.nativescript.org/webpack
 
   return webpack.resolveConfig();
 };
