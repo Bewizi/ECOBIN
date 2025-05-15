7bc8842fee3acc56885e153d3598684a32e52fa5
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-15 16:02:18 +0100

    feat: add a snackbar

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { createApp } from "nativescript-vue";
 
 import "@/app.css";
+import "@nativescript-community/ui-material-snackbar";
+import "@nativescript-community/ui-material-core";
 
 import App from "@/App.vue";
 
