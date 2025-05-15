3d979be0ace0928e373ea6450f5154d6be84bd8c
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-15 20:15:03 +0100

    feat:add toastify message to show error and success message

--- src/Auth/SignUp.vue
+++ src/Auth/SignUp.vue
@@ -18,6 +18,7 @@ import Setup from "~/pages/Setup.vue";
 import api from "~/services/api_service";
 import { navigate } from "~/utils/navigation";
 import Login from "./Login.vue";
+import { Toast } from "~/services/toast_service";
 
 const completeSetup = () => {
   $navigateTo(Home, { clearHistory: true });
@@ -32,10 +33,13 @@ const showPassword = ref<boolean>(false);
 
 const form = ref({
   firstname: "",
+  lastname: "",
   email: "",
-  // phone: "",
+  phoneNumber: "",
   password: "",
 });
+
+const errors = ref<Record<string, string>>({});
 
 const handleFormSubmit = async () => {
   try {
@@ -43,39 +47,44 @@ const handleFormSubmit = async () => {
 
     const respone = await api.auth.register({
       firstname: form.value.firstname,
+      lastname: form.value.lastname,
+      phoneNumber: form.value.phoneNumber,
       email: form.value.email,
       password: form.value.password,
     });
 
     if (respone.status === 201) {
       console.log("Registration Successful", respone);
+      await Toast.showSuccess("Registration Successful");
+
+      form.value.firstname = "";
+      form.value.lastname = "";
+      form.value.email = "";
+      form.value.phoneNumber = "";
+      form.value.password = "";
+
+      const viewModal = () => {
+        $showModal(SuccessModal, {
+          fullscreen: false,
+          animated: true,
+          stretched: true,
+          transition: {
+            name: "fade",
+            duration: 300,
+            curve: "easeIn",
+          },
+          props: {
+            completeSetup,
+            skipForNow,
+          },
+        });
+      };
+
+      viewModal();
     }
-
-    form.value.firstname = "";
-    form.value.email = "";
-    // form.value.phone = "";
-    form.value.password = "";
-
-    const viewModal = () => {
-      $showModal(SuccessModal, {
-        fullscreen: false,
-        animated: true,
-        stretched: true,
-        transition: {
-          name: "fade",
-          duration: 300,
-          curve: "easeIn",
-        },
-        props: {
-          completeSetup,
-          skipForNow,
-        },
-      });
-    };
-
-    viewModal();
   } catch (error) {
     console.log("Registration Failed", error);
+    await Toast.showError("Registration Failed");
   }
 };
 
@@ -108,14 +117,23 @@ const navigateToSignup = () => {
 
         <!-- Form Fields-->
         <StackLayout class="px-6 pt-5">
-          <!-- Full Name -->
-          <Label text="Full Name" class="text-black mb-2" fontSize="20" />
+          <!-- Fist Name -->
+          <Label text="First Name" class="text-black mb-2" fontSize="20" />
           <TextField
             v-model="form.firstname"
             hint="Enter your full name"
             class="mb-4 p-4 border-2 border-[#C1C8D6] rounded-xl placeholder:text-[#7E8798]"
           />
-          <!-- Full Name -->
+          <!-- First Name -->
+
+          <!-- Last Name -->
+          <Label text="Last Name" class="text-black mb-2" fontSize="20" />
+          <TextField
+            v-model="form.lastname"
+            hint="Enter your full name"
+            class="mb-4 p-4 border-2 border-[#C1C8D6] rounded-xl placeholder:text-[#7E8798]"
+          />
+          <!-- Last Name -->
 
           <!-- Email Address -->
           <Label text="Email Address" class="text-black mb-2" fontSize="20" />
@@ -128,13 +146,13 @@ const navigateToSignup = () => {
           <!-- Email Address -->
 
           <!-- Phone Number -->
-          <!-- <Label text="Phone Number" class="text-black mb-2" fontSize="20" />
+          <Label text="Phone Number" class="text-black mb-2" fontSize="20" />
           <TextField
-            v-model="form.phone"
+            v-model="form.phoneNumber"
             hint="Enter your phone number"
             keyboardType="phone"
             class="mb-4 p-4 border-2 border-[#C1C8D6] rounded-xl placeholder:text-[#7E8798]"
-          /> -->
+          />
           <!-- Phone Number -->
 
           <!-- Password -->
