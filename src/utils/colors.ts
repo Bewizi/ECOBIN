0d80adbb526fd48968e1a244b17314faaa0e3583
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-17 11:09:22 +0100

    feat:add colors to use default colors than creating typing them in my code

---
+++ src/utils/colors.ts
@@ -0,0 +1,12 @@
+const hex = "#54B469";
+const white = "#ffff";
+const _ =
+  "bg-[#54B469] bg-[#ffffff]  text-[#54B469] border-[#54B469] outline-[#54B469] text-[#fff]";
+
+export const colors = {
+  bgClass: `bg-[${hex}]`,
+  button: {
+    primary: `border-2 border-[${hex}] rounded-xl mb-5 bg-white text-[${hex}]`,
+    secondary: `border-2 border-[${white}] rounded-xl mb-5 bg-[${hex}] text-white`,
+  },
+};
