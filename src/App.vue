c156e5fa3531be87a5b9d1b9af4cd4f40d872fb5
Bewizi <toluwalase.bewaji@yahoo.com>
2025-05-27 05:27:20 +0100

    refactor: replace Requests component with OnboardingSlides in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script lang="ts" setup>
 import OnboardingSlides from "./components/onboarding/OnboardingSlides.vue";
 import { Frame, RootLayout } from "@nativescript/core";
-import Setup from "./pages/Setup.vue";
-import Profile from "./pages/Profile.vue";
-import Activity from "./pages/Activity.vue";
-import Requests from "./pages/Requests.vue";
 </script>
 
 <template>
   <RootLayout>
     <Frame>
-      <Requests />
+      <OnboardingSlides />
     </Frame>
   </RootLayout>
 </template>
